a2aa2a39cee02eece5b132e0b4f668b2f649236e
sandy-1995 <sksanthosh1996@gmail.com>
2023-05-09 00:30:38 +0530

    Data ingestion completed

--- main.py
+++ main.py
@@ -3,21 +3,19 @@ from src.logger import logging
 import os
 import sys
 from src.utils import get_collection_as_dataframe
-
-
-# def test_logger_and_expection():
-#     try:
-#        logging.info("Starting the test_logger_and_exception")
-#        result = 3/0
-#        print(result)
-#        logging.info("Stoping the test_logger_and_exception")
-#     except Exception as e:
-#        logging.debug(str(e))
-#        raise InsuranceException(e, sys)
-    
+from src.entity.config_entity import DataIngestionConfig
+from src.entity.config_entity import TrainingPipelineConfig
+from src.components.data_ingestion import DataIngestion
+from src.entity.artifacts_entity import DataIngestionArtifact
+  
 if __name__=="__main__":
     try:
-        get_collection_as_dataframe(database_name="INSURANCE", collection_name="INSURANCE_PROJECT")
+        # get_collection_as_dataframe(database_name="INSURANCE", collection_name="INSURANCE_PROJECT")
         # test_logger_and_expection()
+        training_pipelne_config =TrainingPipelineConfig()
+        data_ingestion_config =DataIngestionConfig(training_pipeline_config=training_pipelne_config)
+        print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        data_ingestion_artifacts = data_ingestion.initiate_data_ingestion()
     except Exception as e:
         print(e)
